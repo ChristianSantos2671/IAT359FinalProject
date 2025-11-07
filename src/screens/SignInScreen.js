c6cf53e980b51fe748d0157208d17f48a5131112
stnewaz <samaila1813@gmail.com>
2025-11-06 17:41:49 -0800

    Adding sam's files

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, TouchableOpacity, Alert, TextInput, Text, View } from 'react-native';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import {firebase_auth} from  '../util/firebaseConfig';
+
+export default function SignInScreen({navigation}) {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false);
+
+  /*const handleSignUp = async () => {
+      try {
+          setLoading(true);
+          await createUserWithEmailAndPassword(firebase_auth, email.trim(), password);
+          Alert.alert("Account Created");
+      } catch (e) {
+          Alert.alert("Failed to create account", e.message);
+      } finally {
+          setLoading(false);
+      }
+  }; */
+
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(firebase_auth, email.trim(), password);
+      Alert.alert("Logged in successfully!");
+    } catch (e) {
+      Alert.alert("Login Failed", e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      <View>
+        <Text style={styles.h1} > Login to your account! </Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value = {email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+          autoCapitalize="none"
+        />
+
+        <TextInput
+          placeholder="Password"
+          value = {password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.primaryButton}
+        >
+          <Text style={styles.primaryButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('SignUp')}
+          style={styles.secondaryButton}
+        >
+          <Text style={styles.secondaryButtonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+
+  h1: {
+     color: 'green',
+     fontWeight: '700',
+     fontSize: 35
+  },
+
+  inputContainer: {
+    width: '80%',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#fff',
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    marginVertical: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
+  },
+  buttonContainer: {
+    width: '80%',
+    alignItems: 'center',
+  },
+  primaryButton: {
+    backgroundColor: 'green',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  secondaryButton: {
+    borderColor: 'green',
+    padding: 15,
+  },
+
+  primaryButtonText: {
+    color: 'white',
+    fontWeight: '700',
+  },
+  secondaryButtonText: {
+    color: 'green',
+    fontWeight: '700',
+  },
+});
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,20 @@
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+import { getFirestore} from 'firebase/firestore';
+
+  // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyB2Ng7zEwJpgB_0G9MaSRNCfPnVayD9Qws",
+   authDomain: "mycookbook-d22ac.firebaseapp.com",
+   projectId: "mycookbook-d22ac",
+   storageBucket: "mycookbook-d22ac.appspot.com",
+   messagingSenderId: "303284188379",
+   appId: "1:303284188379:web:91962519b6a5e6878588fe"
+ };
+
+  // Initialize Firebase
+export const mycookbook_app = initializeApp(firebaseConfig);
+export const firebase_auth = getAuth(mycookbook_app);
+export const db = getFirestore(mycookbook_app);
+
